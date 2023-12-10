a9b79ee15be62174dc6499cdd1e995ecdc968b41
Lucas Cota <lucas.a.cota@gmail.com>
2023-12-10 10:12:38 -0300

    create controller and model from user

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,22 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UsersController {
+  public async index({}: HttpContextContract)  { //lista usuários
+
+  }
+
+  public async store({}: HttpContextContract) { // armazena
+  }
+
+  public async show({}: HttpContextContract) { // mostra usuário
+
+  }
+
+  public async update({}: HttpContextContract) { // atualiza 
+
+  }
+
+  public async destroy({}: HttpContextContract) { // deleta
+
+  }
+}
